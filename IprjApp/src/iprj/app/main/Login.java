545dce8ed1aebd88864f9189d12cc2fda8dbb9d8
Paulocajr <paulocajr@live.com>
2015-01-08 12:04:04 -0200

    Update Login.java

--- IprjApp/src/iprj/app/main/Login.java
+++ IprjApp/src/iprj/app/main/Login.java
@@ -1,7 +1,19 @@
 package iprj.app.main;
 
+/*
+  * Login.java
+  * Versão: <v2.0>
+  * Data de Criação : 19/09/2014
+  * Copyright (C) 2014 Paulo cabral
+  * Instituto Politécnico do Estado do Rio de Janeiro
+  * IPRJ - http://www.iprj.uerj.br
+  * Classe responsável pela tela de Login do aplicativo
+  * Todos os direitos reservados.
+ */
+ 
+ //Imports
+ 
 import iprj.app.fragments.FragmentChangeActivity;
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -14,10 +26,8 @@ import java.net.URL;
 import java.util.Calendar;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
@@ -26,11 +36,9 @@ import com.parse.ParsePush;
 import com.parse.ParseUser;
 import com.parse.PushService;
 import com.parse.SignUpCallback;
-
 import android.app.Application;
 import net.simonvt.numberpicker.NumberPicker;
 import net.simonvt.numberpicker.NumberPicker.OnValueChangeListener;
-
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
@@ -47,7 +55,6 @@ import com.iprjappteste.data.Periodos;
 import com.iprjappteste.data.Usuario;
 import com.parse.PushService;
 import com.sinch.messagingtutorial.app.MessageService;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -79,13 +86,10 @@ public class Login extends Activity  {
 	 private String[] id;
 	 private static String APP_ID ="1458439531078582";
 	  @SuppressWarnings("deprecation")
-     private Facebook facebook = new Facebook(APP_ID);
+         private Facebook facebook = new Facebook(APP_ID);
 	 public String User_name;
 	 public CheckBox check3;
-	  private Intent serviceIntent;
- 	 
- 	
-    
+	 private Intent serviceIntent;
 	 private Calendar now = Calendar.getInstance();
 	 int year = now.get(Calendar.YEAR);
 	 
@@ -93,124 +97,126 @@ public class Login extends Activity  {
      public void onCreate(Bundle savedInstanceState) {
 	     super.onCreate(savedInstanceState);
 	     
-		    final MySQLiteHelper db = new MySQLiteHelper(Login.this);
-		    
-		    Parse.initialize(this, "8FttRlqaAUPabxXzuLeTMbmgAA389L7oVbFaSqjj", "6sFwktakiPYSDg1NKzgP18yanSjlB9CQKFGUOShs");
-			
-		 	 ParseInstallation.getCurrentInstallation().saveInBackground();	
-		 	 
-		 	serviceIntent = new Intent(this, MessageService.class);
-		 	
-		 	  ParseUser currentUser = ParseUser.getCurrentUser();
-		        if (currentUser != null) {
-		            startService(serviceIntent);
-		        }
+	     
+	               // inicializa banco de dados
+		       final MySQLiteHelper db = new MySQLiteHelper(Login.this);
+		 
+	       
+	                overridePendingTransition(R.anim.fadein, R.anim.fadeout);	     
+		        requestWindowFeature(Window.FEATURE_NO_TITLE);
+		        
+		        //carrega o layout a partir do XML
+		        setContentView(R.layout.login);		  
 		  
-	        overridePendingTransition(R.anim.fadein, R.anim.fadeout);	     
-		    requestWindowFeature(Window.FEATURE_NO_TITLE);
-		    setContentView(R.layout.login);		  
-		  
-			
+		        //icinializando o Parse
+		        Parse.initialize(this, "8FttRlqaAUPabxXzuLeTMbmgAA389L7oVbFaSqjj", "6sFwktakiPYSDg1NKzgP18yanSjlB9CQKFGUOShs");
+			ParseInstallation.getCurrentInstallation().saveInBackground();	
+		 
+		 	// salvando valores default de avisos e lembrete
 			db.addAviso(new Avisos_Caip("Sem novos avisos!",""));			
 			db.addAviso_Prof(new Avisos_Prof("Sem novos avisos!",""));
 			db.addAtividade(new Atividade("Adicione um Lembrete!",""));
 			
+			//declarando itens do XML
 			final Button  Button = (Button) findViewById(R.id.button1);
+			
+			// NumberPicker com as opções de período
 			final NumberPicker np = (NumberPicker)findViewById(R.id.numberPicker);
 			final NumberPicker np_2 = (NumberPicker)findViewById(R.id.numberPicker_2);
 			
+			//valor máximo de período
 			np.setMaxValue(10);
-		    np.setMinValue(1);
-		    np.setFocusable(true);
-		    np.setFocusableInTouchMode(true);
-		    
-		    np_2.setMaxValue(10);
-		    np_2.setMinValue(1);
-		    np_2.setFocusable(true);
-		    np_2.setFocusableInTouchMode(true);
+			//valor mínimo de período
+		        np.setMinValue(1);
+		        np.setFocusable(true);
+		        np.setFocusableInTouchMode(true);
+		        
+		    	//valor máximo de período
+		        np_2.setMaxValue(10);
+		        //valor mínimo de período
+		        np_2.setMinValue(1);
+		        np_2.setFocusable(true);
+		        np_2.setFocusableInTouchMode(true);
 
-           	num_per2 = Integer.toString(np.getMinValue());
-           	num_per2 = "0"+num_per2;
-           	nome_per = num_per2+"� periodo";
-           	nome_per2 = nome_per; 
-           	
-        
+                 	num_per2 = Integer.toString(np.getMinValue());
+                 	num_per2 = "0"+num_per2;
+                	nome_per = num_per2+"° periodo";
+                	nome_per2 = nome_per; 
 	         		  	            
-	        np.setOnValueChangedListener(new OnValueChangeListener() {
+	         		  	            
+	               np.setOnValueChangedListener(new OnValueChangeListener() {
 	                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 	                    // do something here
 	               
-	                	 final int pickedValue = np.getValue();
+	                	  final int pickedValue = np.getValue();
 	                	  num_per = Integer.toString(pickedValue);
-	                	 	 //Toast.makeText(Login.this,num_per, Toast.LENGTH_SHORT).show();
+	                	 
 		                  if(num_per.equals("10")) {	
 		                	  
 		                	  num_per2 = num_per;
-		                	  nome_per = num_per+"� periodo";
+		                	  nome_per = num_per+"° periodo";
 		                	  nome_per2 = nome_per;
-
-		                      }
+		                             }
 		                  
 		                         else {
 		                            num_per = "0"+num_per;
-	       	                        nome_per = num_per+"� periodo";	
+	       	                            nome_per = num_per+"° periodo";	
 	       	        	            num_per2 = num_per;
 	       	        	            nome_per2 = nome_per;
-		                            }
+		                           }
 	       	           	      }
 	                	                
 	                });
 	           
-	           
-	           np_2.setOnValueChangedListener(new OnValueChangeListener() {
-	                public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-	                    // do something here
+	              //retorna o valor mudado no numberpicker
+	               np_2.setOnValueChangedListener(new OnValueChangeListener() {
+	                 public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+	                    
 	               
 	                	 final int pickedValue_2 = np_2.getValue();
 	                	  num_per3 = Integer.toString(pickedValue_2);
-	                	 	 //Toast.makeText(Login.this,num_per, Toast.LENGTH_SHORT).show();
+	                	 
 		                	 
 	                	  if(num_per3.equals("10")){ 	 
 		                	   
 	                		  num_per4 = num_per3;
-	                		  nome_per3 = num_per3+"� periodo";
+	                		  nome_per3 = num_per3+"° periodo";
 	                		  nome_per4 = nome_per3;
 	                	          }
 	                	   
-	                	             else {
+	                	   else {
 	                             	  num_per3 = "0"+num_per3;
-	       	                          nome_per3 = num_per3+"� periodo";	
-	       	        	              num_per4 = num_per3;
-	       	        	              nome_per4 = nome_per3;
+	       	                          nome_per3 = num_per3+"° periodo";	
+	       	        	          num_per4 = num_per3;
+	       	        	          nome_per4 = nome_per3;
 	                	           }
 	       	           
 	                           }
 	                
 	                
 	                        });
-	           
+	                        
+	        //declarando Ckekbox do layout   
 	        check1 = (CheckBox) findViewById(R.id.checkBox1);
 	        check2 = (CheckBox) findViewById(R.id.checkBox2);
 	        check3 = (CheckBox) findViewById(R.id.checkBox3);
+	        
 	        check3.setOnClickListener(new View.OnClickListener() {
 	                public void onClick(View v) {
-	                    // Perform action on clicks, depending on whether it's now checked
+	                	
+	                    //verificando o status da checkbox
 	                    if (((CheckBox) v).isChecked()) {
 	                    	
-	                      np_2.setVisibility(View.VISIBLE);
-	                      
+	                        np_2.setVisibility(View.VISIBLE);
 	                        num_per4 = Integer.toString(np.getMinValue());
 	                        num_per4 = "0"+num_per4;
-	              	        nome_per3 = num_per4+"� periodo";
+	              	        nome_per3 = num_per4+"° periodo";
 	              	        nome_per4 = nome_per3;
-	              	        
 	                                 }	                    
-	                    
-	       	        	           else {
-	       	        		  
-	       	        		        np_2.setVisibility(View.GONE);
-		                            num_per4 = null;
-		              	            nome_per4 = null;
+	                     else {
+	       	                 np_2.setVisibility(View.GONE);
+		                 num_per4 = null;
+		              	 nome_per4 = null;
 	       	        	        }
 	                          }
 	                     });
@@ -227,86 +233,89 @@ public class Login extends Activity  {
 				           	    	   
 
 			                		    	  Intent intent = new Intent();
-			                		    	  db.addCurso(new Curso("Engenharia Mec�nica"));  			                		    	
+			                		    	  db.addCurso(new Curso("Engenharia Mecânica"));  			                		    	
 			                		    	  db.addPeriodo(new Periodos(nome_per2,num_per2));
 			                		    	  db.addPeriodo(new Periodos(nome_per4,num_per4));
 			                		    	  ParsePush.subscribeInBackground("Mecanica");
-						                      intent.setClass(Login.this,FragmentChangeActivity.class);
-						                      intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-						                      startActivity(intent);
-						                      finish();		
-						                      
+						                  intent.setClass(Login.this,FragmentChangeActivity.class);
+						                  intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+						                  startActivity(intent);
+						                  finish();		
+						                     
 				                	             }
 				                	  
-				                	                else {
-				                		                   if (check2.isChecked() && !check1.isChecked() ) {
+				                	     else if (check2.isChecked() && !check1.isChecked() ) {
 				                		    
-				                		       Intent intent = new Intent();
-
-				                		       db.addCurso(new Curso("Engenharia de Computa��o"));
+				                		   Intent intent = new Intent();
+				                		   db.addCurso(new Curso("Engenharia de Computação"));
 				                	           db.addPeriodo(new Periodos(nome_per2,num_per2));
 				                	           db.addPeriodo(new Periodos(nome_per4,num_per4));
 				                	           ParsePush.subscribeInBackground("Computacao");
-							                   intent.setClass(Login.this,FragmentChangeActivity.class);
-							                   intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);							    
-							                   startActivity(intent);
-							                   finish();
+							           intent.setClass(Login.this,FragmentChangeActivity.class);
+							           intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);							    
+							           startActivity(intent);
+							           finish();
 							                   
 				                		         }
 				                		    	                                           
-				                		            else if (check2.isChecked() && check1.isChecked()) {
+				                	      else if (check2.isChecked() && check1.isChecked()) {
 				                		    				                		        
 				                		    	   Toast.makeText(getApplicationContext(), "Selecione apenas um Curso", Toast.LENGTH_SHORT).show();
 				                		    	   
-				                		                   }
-				                		    			              						                		    
+				                		        }
 				                		    			               		    
-				                		                       else {
+				                	       else {
 				                		    	
-				                		    	               Toast.makeText(getApplicationContext(), "Selecione o Curso", Toast.LENGTH_SHORT).show();
-				                		                    }
+				                		    	   Toast.makeText(getApplicationContext(), "Selecione o Curso", Toast.LENGTH_SHORT).show();
+				                		        }
 
-				                		                  }
-				                                       }   
-				                                   });
+				                		     }
+				                                  }   
+				                             });
        
-			
+			//definiindo URL que será passada ao médoto que fará o Download do Calendário acadêmico
 			 String url = "http://www.uerj.br/comunidade/arquivos/ca"+year+"/ca"+year+".pdf";
 			 WebView webView = (WebView) findViewById(R.id.webView1);
-	         webView.setWebViewClient(new WebViewClient());
+	                 webView.setWebViewClient(new WebViewClient());
 	          
 
-				
+		         //definiindo URL que será passada ao médoto que fará o Download do Guia de ramais	
 			 final String url_2 = "http://www.iprj.uerj.br/images/arquivos/coapo/Alfab%C3%A9tica.pdf";
 			 WebView webView_2 = (WebView) findViewById(R.id.webView2);
-	         webView_2.setWebViewClient(new WebViewClient());
-	 
-	            webView.setDownloadListener(new DownloadListener() {
-	            @Override
-	            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {             
+	                 webView_2.setWebViewClient(new WebViewClient());
+	                 
+	                //webview que carrerá a página de download do arquivo PDF do Calendário
+	                 webView.setDownloadListener(new DownloadListener() {
+	                 @Override
+	                 public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {             
 	                //start download
 	                DownloadPDF downloadPDF = new DownloadPDF();
 	                downloadPDF.execute(url,userAgent,contentDisposition);
 	            }                     
 	        });
 	      
-	            webView.getSettings().setJavaScriptEnabled(true);
-	            webView.loadUrl(url);
-	            webView_2.setDownloadListener(new DownloadListener() {
+	               webView.getSettings().setJavaScriptEnabled(true);
+	               webView.loadUrl(url);
+	               
+	                //webview que carrerá a página de download do arquivo PDF do Guia de Ramais
+	               webView_2.setDownloadListener(new DownloadListener() {
 	               @Override
-	              public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {             
+	               public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {             
 	                //start download
 	            	Download_Ramal downloadPDF = new Download_Ramal();
 	                downloadPDF.execute(url_2,userAgent,contentDisposition);
 	            }                     
 	        });
 	      
-	        webView_2.getSettings().setJavaScriptEnabled(true);	    
-	        webView_2.loadUrl(url_2);    
+	              webView_2.getSettings().setJavaScriptEnabled(true);	    
+	              webView_2.loadUrl(url_2);    
 	        
 
      }
 
+//Método que faz o download do arquivo a partir da URL passada
+//Faz o download do Calendário Acadêmico
+//Cria uma pasta no SD card para salvar os arquivos .pdf
    
      
     private class DownloadPDF extends AsyncTask<String, Integer, String> {                       
@@ -366,7 +375,9 @@ public class Login extends Activity  {
 		}
     }
 	
-    
+//Método que faz o download do arquivo a partir da URL passada
+//Faz o download do Guia de Ramais
+//Cria uma pasta no SD card para salvar os arquivos .pdf    
     
     private class Download_Ramal extends AsyncTask<String, Integer, String> {                       
         @Override
@@ -392,7 +403,7 @@ public class Login extends Activity  {
                 }       
                
 
-                File outputFile = new File(myDir, "Alfab�tica.pdf");
+                File outputFile = new File(myDir, "Alfabética.pdf");
                 InputStream input   = new BufferedInputStream(connection.getInputStream());
                 OutputStream output = new FileOutputStream(outputFile);
                 
