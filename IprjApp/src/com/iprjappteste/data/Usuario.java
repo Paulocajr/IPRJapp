475756114ea73b355f97ec6d7601d6ac7632c19b
Paulocajr <paulocajr@live.com>
2015-01-08 15:00:27 -0200

    Update Usuario.java

--- IprjApp/src/com/iprjappteste/data/Usuario.java
+++ IprjApp/src/com/iprjappteste/data/Usuario.java
@@ -1,15 +1,27 @@
 package com.iprjappteste.data;
 
+
+/*
+  * Usuario.java
+  * Versão: <v2.0>
+  * Data de Criação : 10/09/2014
+  * Copyright (C) 2014 Paulo cabral
+  * Instituto Politécnico do Estado do Rio de Janeiro
+  * IPRJ - http://www.iprj.uerj.br
+  * Classe responsável pela criação da entidade Usuário no banco de dados
+  * Todos os direitos reservados.
+ */
+
 public class Usuario {
 	
+//Declaração de Varriáveis
 	   public int id;
 	   public String id_facebook;
-	    public String title;
-	  
-	 
-	    public Usuario(){}
-	 
-	    public Usuario(String title, String id_facebook ) {
+	   public String title;
+	   public Usuario(){}
+	   
+//Objeto Usuario	   
+	   public Usuario(String title, String id_facebook ) {
 	        super();
 	        this.title = title;
 	        this.id = id;
@@ -21,7 +33,7 @@ public class Usuario {
 	        return "Curso [id=" + id + ", title=" + title +" ,id_facebook = "+id_facebook+"]";
 	    }
 
-
+//Métodos get e set usados para retornar e setar os valores
 		public void setId(int id) {
 			// TODO Auto-generated method stub
 			this.id = id;
